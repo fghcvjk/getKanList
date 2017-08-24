3c3e1a1dfb8b506d4c13f3f4eaa5ad758366ced9
pony <347444778@qq.com>
2017-08-24 16:11:24 +0800

    增加注释

--- getKanList.py
+++ getKanList.py
@@ -9,10 +9,11 @@ import urllib2
 import re
 import chardet
 
-getKanStr = '[]'
+getKanStr = '[]' #已经开图鉴了的船不显示
 getKanList = eval(getKanStr)
-url = 'https://zh.kcwiki.org/wiki/%E8%88%B0%E5%A8%98%E5%9B%BE%E9%89%B4'
+url = 'https://zh.kcwiki.org/wiki/%E8%88%B0%E5%A8%98%E5%9B%BE%E9%89%B4' #数据使用舰娘百科，日文wiki有谷歌验证
 
+#获取网页数据
 try:
     request = urllib2.Request(url)
     response = urllib2.urlopen(request)
@@ -23,6 +24,7 @@ except urllib2.URLError, e:
     if hasattr(e,"reason"):
         print e.reason
 
+#抓出需要的数据
 pattern = re.compile(r'(?<=<img alt=").+?(?=" src=")')
 kanNameDataList = pattern.findall(urlData)
 errorkanNames = {}
@@ -36,8 +38,9 @@ for index, kanName in enumerate(kanNameDataList):
                 kanNameList.append(kanName)
     except:
         errorkanNames[index] = kanName
-print 'error data:', errorkanNames
+
 #获取有问题的手动处理一下
+print 'error data:', errorkanNames
 for addName in ['岚', '萩风']:
     if addName not in kanNameList:
         kanNameList.append(addName)
